e6639a0106187dcca52236edc95fc08d68064fa7
Karl Joseph C. Kangleon <kangleon.kjc@AD.SPROBE.PH>
2019-06-06 17:10:13 +0800

    Add firebase environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCqF8-gM-tYcoUVPHD1s_SI7Cc9VCZzIaU",
+    authDomain: "complaint-inventory.firebaseapp.com",
+    databaseURL: "https://complaint-inventory.firebaseio.com",
+    projectId: "complaint-inventory",
+    storageBucket: "complaint-inventory.appspot.com",
+    messagingSenderId: "102205298264"
+  }
 };
